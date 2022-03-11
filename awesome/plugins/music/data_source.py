a8f7599b225c1351861dc743af95629002cec155
luyueti <201811011123@mail.bnu.edu.cn>
2022-03-11 19:29:04 +0800

    linux test v2

--- awesome/plugins/music/data_source.py
+++ awesome/plugins/music/data_source.py
@@ -8,7 +8,7 @@ import time
 
 
 async def get_song_id(song: str):
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
